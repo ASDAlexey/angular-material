2647d30b5676d12a91476d4c5410d379621d116b
Alexey Popov <asdalexey@yandex.ru>
2018-07-10 17:39:29 +0300

    #3 add basic components

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  exports: [MatToolbarModule],
+  exports: [
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+  ],
   declarations: []
 })
 export class AppMaterialModule {
