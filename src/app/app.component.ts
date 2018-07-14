639610eabf6e37964cd61000d1222b8064dc9819
Alexey <asdalexey@yandex.ru>
2018-07-14 18:39:45 +0300

    #4 mat-toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { fromEvent, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+export const SCOLL_CONTAINER = '.mat-dawer-content';
+export const PIMARY_TEXT_THRESHOLD = 22;
+export const PIMARY_SHADOW_THRESHOLD = 78;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  popText: boolean;
+  applyShadow: boolean;
+  subsctiption = new Subject();
   title = 'app';
+
+  ngOnInit() {
+    fromEvent(document, 'scroll').pipe(takeUntil(this.subsctiption)).subscribe(() => this.determineHeader(window.pageYOffset));
+  }
+
+  determineHeader(top: number) {
+    console.log(top);
+    this.popText = top >= PIMARY_TEXT_THRESHOLD;
+    this.applyShadow = top >= PIMARY_SHADOW_THRESHOLD;
+  }
+
+  ngOnDestroy() {
+    this.subsctiption.next();
+    this.subsctiption.complete();
+
+  }
 }
