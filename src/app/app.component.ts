fe99caa452e062a31d0d830420243668c65d4ea7
Alexey <asdalexey@yandex.ru>
2018-07-15 10:31:16 +0300

    #6 custom error matcher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,33 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { fromEvent, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-import { MatIconRegistry } from '@angular/material';
-import { DomSanitizer } from '@angular/platform-browser';
+import { ErrorStateMatcher, MatIconRegistry } from '@angular/material';
+import { FormControl, FormGroupDirective, NgForm } from '@angular/forms';
 
 export const SCOLL_CONTAINER = '.mat-dawer-content';
 export const PIMARY_TEXT_THRESHOLD = 22;
 export const PIMARY_SHADOW_THRESHOLD = 78;
 
+export class CustomErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    return !!(control && control.invalid && control.dirty);
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [CustomErrorStateMatcher],
 })
 export class AppComponent implements OnInit, OnDestroy {
   popText: boolean;
   applyShadow: boolean;
   subsctiption = new Subject();
+  customErrorStateMatcher = new CustomErrorStateMatcher();
 
   ngOnInit() {
     fromEvent(document, 'scroll').pipe(takeUntil(this.subsctiption)).subscribe(() => this.determineHeader(window.pageYOffset));
